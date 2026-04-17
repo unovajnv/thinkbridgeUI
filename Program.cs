d3f6205859bc422af9ed25c887d9bab17a83fb8a
umesh <umesh2018pra@gmail.com>
2026-04-17 15:46:09 +0530

    added beutifiy logic for swagger

--- Program.cs
+++ Program.cs
@@ -3,7 +3,11 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
